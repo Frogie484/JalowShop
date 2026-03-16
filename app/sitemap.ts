23886ef3463315d27ae7e43bc082f2eb3cfd5a8b
Матвей Курточаков <matvey@MacBook-Air-Matvej.local>
2026-03-16 09:25:04 +0300

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://jalowshop.ru",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://jalowshop.ru/catalog",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://jalowshop.ru/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://jalowshop.ru/contacts",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://jalowshop.ru/shipping-payment",
+      lastModified: new Date(),
+    },
+  ];
+}
